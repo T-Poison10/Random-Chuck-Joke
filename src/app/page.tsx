97a944322e862b86db1e59c2495d2c5914259850
T-Poison10 <dipansilwal10@gmail.com>
2024-01-29 22:33:23 +0545

    updated progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,31 +38,36 @@ export default function Home() {
 	};
 
 	return (
-		<div className="container">
+		<div className="grid place-items-center h-screen">
 			<Head>
-				<title>Random Jokes Generator</title>
+				<title >Random Jokes Generator</title>
 				<meta name="description"
 					content="Random Jokes Generator" />
 			</Head>
 
-			<div className="app">
+			<div className="content-center">
 				<div className="card">
-					<h1 className="heading">
+					<h1 className="text-3xl font-bold
+					grid place-items-center">
 						Random Chuck-Joke Generator
 					</h1>
 					<div className="jokeContainer">
 						{error ? (
 							<p className="errorText">{error}</p>):(
 							<>
-								<p className="jokeText">{joke}</p>
-								<button className="copyButton"
+								<p className="bg-yellow-300
+								text-sky-400
+								pl-5 pr-5 pt-5 pb-5 rounded">{joke}</p>
+								<button className="bg-transparent hover:bg-white text-white font-semibold hover:text-black py-2 px-4 border border-white hover:border-white rounded
+								grid place-items-center"
 									onClick={handleCopyClick}>
 									{copySuccess ? 'Copied!' : 'Copy'}
 								</button>
 							</>
 						)}
 					</div>
-					<button className='button' onClick={getJoke}>
+					<button className='bg-transparent hover:bg-white text-white font-semibold hover:text-black py-2 px-4 border border-white hover:border-white rounded
+					grid place-items-center' onClick={getJoke}>
 						Get a Joke
 					</button>
 				</div>
