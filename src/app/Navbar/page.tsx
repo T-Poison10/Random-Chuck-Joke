ae6ae67791bf9b0f58ec3f8d60f90726b1167838
T-Poison10 <dipansilwal10@gmail.com>
2024-01-31 11:46:36 +0545

    updated navbar

--- src/app/Navbar/page.tsx
+++ src/app/Navbar/page.tsx
@@ -5,13 +5,13 @@ export default function Nav()
         
 
 <nav className=" dark:bg-purple-800 rounded-full m-3">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between p-4">
+  <div className="max-w-screen-xl flex items-center justify-between p-4">
     <a href=".." className="flex items-center">
         <img src="https://api.chucknorris.io/img/chucknorris_logo_coloured_small@2x.png" className="h-8" alt="Chuck-Logo" />
-        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-black">Chuck-Joke-Generator</span>
+        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-black"> Chuck-Joke-Generator</span>
     </a>
     <div>
-      <ul className="font-medium flex flex-col p-4 md:p-0 border-none rounded-lg bg-gray-50  md:mt-0  md:dark:bg-transparent ">
+      <ul className="font-medium flex p-4 md:p-0">
         <li>
           <a href="/About" className="block text-black rounded hover:bg-transparent dark:hover:text-white md:dark:hover:bg-transparent">About</a>
         </li>
